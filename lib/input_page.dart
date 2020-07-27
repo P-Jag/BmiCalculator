71d4f95a5b7fbd797dab6224f10d35115607273c
P-Jag <pavel.jagla@gmail.com>
2020-07-27 15:58:33 +0200

    layout

---
+++ lib/input_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'Reusable_Card.dart';
+
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+const activeCardColor = Color(0xFF1D1E33);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(color: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(color: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(color: activeCardColor),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(color: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(color: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: bottomContainerHeight,
+            margin: EdgeInsets.only(top: 10.0),
+            color: bottomContainerColor,
+            width: double.infinity,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(BmiCalculator());
+
+class BmiCalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: InputPage(),
+    );
+  }
 }
